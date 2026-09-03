7ab56441f1b9a14c106abb91c25c15082d513df6
srplastic2021-web <srplastic2021@gmail.com>
2026-09-04 04:18:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,10 @@
-const CACHE='sr-plastic-v2';
-
+const CACHE='sr-plastic-v3';
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c => c.addAll([
-        './',
-        './index.html',
-        './manifest.webmanifest'
-      ]))
-      .then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.webmanifest'])).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(k => k !== CACHE)
-            .map(k => caches.delete(k))
-        )
-      )
-      .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
