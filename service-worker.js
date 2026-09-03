555c79caaee357750e5a51a06853e0875b36bf47
srplastic2021-web <srplastic2021@gmail.com>
2026-09-04 03:51:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,33 @@
-const CACHE='sr-plastic-v1';
+const CACHE='sr-plastic-v2';
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.webmanifest'])));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll([
+        './',
+        './index.html',
+        './manifest.webmanifest'
+      ]))
+      .then(() => self.skipWaiting())
+  );
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(k => k !== CACHE)
+            .map(k => caches.delete(k))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
